46b99be1fb49219d0e815e7ebac0bc9d31edac17
chikato2887 <suganuma.matsumoto03@gmail.com>
2022-05-29 20:58:20 +0900

    base64 response

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
-export const handler = async () => {
+import chromium from 'chrome-aws-lambda'
+
+// @ts-ignore
+export const handler = async (event) => {
+	let result = null;
+	let browser = null;
+	const questionId = event.queryStringParameters['id']
+
+	try {
+		browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+		let page = await browser.newPage();
+
+    await page.goto(`https://ogp-test-site.vercel.app/og-image/questions/${questionId}`);
+
+    result = await page.screenshot({fullPage: false, type: "png"})
+
+	} catch (error) {
+		return console.error("error has occured: ", error)
+	} finally {
+		if (browser != null) {
+			await browser.close()
+		}
+	}
+
 	const response = {
 		statusCode: 200,
-		body: JSON.stringify("hello from local deployment otsukari")
+		headers: {
+			'Content-Type': 'image/png',
+		},
+		body: result.toString('utf8'),
+		isBase64Encoded: true
 	}
+	console.log("base64: ", result.toString('utf8'))
 	return response
 }
 
-
+if (process.env.NODE_ENV == 'develop') {
+	handler({queryStringParameters: {id: '1'}})
+}
